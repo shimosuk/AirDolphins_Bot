151a6dc7a195a04ea0cff400a3801d5d19b602e9
ryosuke shimomura <r-shimomura@esm.co.jp>
2012-08-28 23:20:55 +0900

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AirDolphinsBot::Application.config.secret_token = '31c4bf3fb6d0c40336a93261e7e46dd4cf293953a7b5be2ea80796e5489c24db1742b84ba32afb6345d640e7336ee2c31de53423c6c0e5e3e63e26307082e288'
