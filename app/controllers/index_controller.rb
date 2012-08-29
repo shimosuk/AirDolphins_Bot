1a0838d5d67eb983650a687ad8d03fff4ef538b3
ryosuke shimomura <r-shimomura@esm.co.jp>
2012-08-29 13:24:41 +0900

    OAuthアクション作成
    認証ボタンでtwitterに転送するアクション

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -3,6 +3,15 @@ class IndexController < ApplicationController
   end
 
   def oauth
+    callback_url = "http://#{request.host_with_port}/callback"
+    request_token = self.class.consumer.get_request_token(oauth_callback: callback_url)
+
+    session[:request_token] = {
+      token: request_token.token,
+      select: request_token.secret,
+    }
+
+    redirect_to request_token.authorize_url
   end
 
   def callback
