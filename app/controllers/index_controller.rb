7e4d03e97434b3c54f4780b39a83a7bbd8543086
ryosuke shimomura <r-shimomura@esm.co.jp>
2012-08-29 13:30:01 +0900

    Callbackアクションの作成
    OAuthで認証できるようになった

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -15,5 +15,29 @@ class IndexController < ApplicationController
   end
 
   def callback
+    if params[:denied]
+      session.delete :oauth
+    else
+      request_token = OAuth::RequestToken.new(
+        self.class.consumer,
+        session[:request_token][:token],
+        session[:request_token][:secret]
+      )
+
+      access_token = request_token.get_access_token(
+        {},
+        oauth_token: params[:oauth_token],
+        oauth_verifier: params[:oauth_verifier]
+      )
+
+      session[:oauth] = {
+        token: access_token.token,
+        secret: access_token.secret
+      }
+    end
+
+    session.delete :request_token
+
+    redirect_to :index
   end
 end
