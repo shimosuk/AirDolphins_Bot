08150d7ac989a876d47a14256d3f37f3c8aa49ef
ryosuke shimomura <r-shimomura@esm.co.jp>
2012-08-29 13:06:58 +0900

    Consumerを取得するラッパーメソッド作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  protected
+  def self.consumer
+    OAuth: :Consumer.new(
+      'comsumerKey',
+      'comsumerSecret',
+      {site: "http://twitter.com"}
+    )
+  end
 end
