1d25ce5afa672f2576ce126fcdbe7e0369c1384a
ryosuke shimomura <r-shimomura@esm.co.jp>
2012-09-23 17:22:55 +0900

    初回ログイン時に呼ばれるモデルのメソッドと認証用コントローラ作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ActiveRecord::Base
   attr_accessible :name, :provider, :screen_name, :uid
+  def self.create_with_omniauth(auth)
+    create! do |user|
+      user.provider = auth["provider"]
+      user.uid = auth["uid"]
+      user.name = auth["user_info"]["name"]
+      user.screen_name = auth["user_info"]["nickname"]
+    end
+  end
 end
