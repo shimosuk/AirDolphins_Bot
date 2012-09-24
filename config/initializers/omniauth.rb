4d7310a2f8888d1e388c60d41de8c5c4442bee4b
ryosuke shimomura <r-shimomura@esm.co.jp>
2012-09-24 12:54:09 +0900

    認証に使うTwitterAppをbot操作で使用するAppと別にした

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, ENV['CONSUMER_KEY'], ENV['CONSUMER_SECRET']
+  provider :twitter, ENV['JOIN_KEY'], ENV['JOIN_SECRET']
 end
