36cf68014c3e14b7b92a86d8d80cbec95037726b
ryosuke shimomura <shimosuk.shimosuke@gmail.com>
2012-11-07 23:58:32 +0900

    初めてのログインは失敗するバグがあった

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,11 +4,7 @@ class SessionsController < ApplicationController
     user = User.find_user(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
     session[:user_id] = user.id
 
-    profile = user.profile
-    if profile.blank? then
-      profile = Profile.create(user.id, user.name, user.uid)
-      redirect_to :edit_profile
-    end
+    profile = user.profile || Profile.create(user.id, user.name, user.uid)
     redirect_to :profile
   end
 
